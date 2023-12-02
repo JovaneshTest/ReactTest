dbc2fd66ffdd706f95b17c72e56119d867ba7ad1
JovaneshPiramoon <jovaneshpiramoon@gmail.com>
2023-12-02 07:08:35 -0800

    remove bg-image from hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,15 +3,7 @@ import Header from "./Header";
 import imageHero from "../Asset/image_hero.png";
 export default function Hero() {
   return (
-    <div
-      className="w-full w-max-[1500px] h-screen relative"
-      style={{
-        backgroundImage: `url(${imageHero})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
+    <div className="w-full w-max-[1500px] h-screen relative">
       <Header></Header>
     </div>
   );
