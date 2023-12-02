8e6dc0c6c53dd7e106a522ead7086f87f0654807
JovaneshPiramoon <jovaneshpiramoon@gmail.com>
2023-12-02 12:14:51 -0800

    this is one better and kinda fix the bug now is responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 
 export default function Header() {
-  const [showMenu, setShowMenu] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div>
       <div className="container  mx-auto flex items-center bg-white  px-4 py-2">
         <a href="#" className="text-4xl uppercase">
           logo
         </a>
-        <nav
-          className={`fixed lg:relative bg-white    right-0 top-0 w-1/2 h-screen lg:h-auto lg:w-auto  flex items-center justify-center lg:block lg:ml-10 lg:translate-x-0 translate-x-full  overflow-hidden duration-300 transition-all   lg:transition-none`}
-        >
-          <ul
-            className={`flex flex-col lg:flex-row lg:space-x-5 space-x-0 uppercase text-2xl text-center space-y-10 lg:space-y-0 w-full h-full  lg:shadow-none  shadow-2xl `}
-          >
+        <nav className={` flex-1 pl-10 hidden lg:flex`}>
+          <ul className={`flex  text-lg lg:text-2xl uppercase space-x-5 `}>
             <li>
               <a href="#">home</a>
             </li>
@@ -34,13 +30,36 @@ export default function Header() {
           <button className="text-2xl bg-red-400 py-2 px-5">SingUp</button>
         </div>
         <button
-          className="z-50 lg:hidden"
+          className="z-50 md:hidden"
           onClick={() => {
             setShowMenu((prev) => !prev);
           }}
         >
           Open
         </button>
+        {/* mobile navbar section  */}
+        <div
+          className={`shadow-2xl shadow-black  fixed  top-0 right-0 w-5/12  h-screen translate-x-full ${
+            showMenu ? "translate-x-0 " : "translate-x-full shadow-none"
+          } transition-all duration-300`}
+        >
+          <ul
+            className={`flex flex-col text-center bg-white w-full h-full items-center justify-center  space-y-10 text-2xl uppercase`}
+          >
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact us</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
