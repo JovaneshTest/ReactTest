17cb8959ca076311336dc0c8b6d2dba320e43860
JovaneshPiramoon <jovaneshpiramoon@gmail.com>
2023-12-01 23:54:57 -0800

    create some sample navbar with out responive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-teal-500 text-4xl text-center">
-        this is just the test{" "}
-      </h1>
+      <div className="container  mx-auto flex items-center ">
+        <a href="#" className="text-4xl uppercase">
+          logo
+        </a>
+        <nav className="ml-10 border-l-4 pl-10">
+          <ul className="text-xl uppercase flex items-center space-x-5">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact us</a>
+            </li>
+          </ul>
+        </nav>
+        {/* btn for the Sing Loging */}
+        <div className="flex-1  items-center flex justify-end space-x-5">
+          <button className="text-2xl">Loging</button>
+          <button className="text-2xl bg-red-400 py-2 px-5">SingUp</button>
+        </div>
+      </div>
     </>
   );
 }
